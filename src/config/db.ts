0a6059c947779452c78019ac9b25bad433a67bf9
olcayusta <olcayusta02@gmail.com>
2020-09-11 23:17:09 +0300

    change db name... movie_streaming to folivera and depend.. updates...

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,9 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    host: 'localhost',
     user: 'postgres',
     password: '123456',
-    database: 'movie_streaming'
+    database: 'folivera'
 })
 
 pool.query('SELECT NOW()').then(({rows}) => {
